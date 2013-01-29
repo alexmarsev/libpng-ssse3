a15f669662d2a09e3bf20ce3afa668d1147262ad
Alex Marsev <alex.marsev@gmail.com>
2013-01-29 10:07:46 +0300

    Enable 48/64bpp ssse3 optimized filters

--- ssse3/ssse3_init.c
+++ ssse3/ssse3_init.c
@@ -11,15 +11,27 @@
 
 void png_init_filter_functions_ssse3(png_structp pp, unsigned int bpp) {
 	if (png_check_cpu_for_ssse3()) {
-		pp->read_filter[PNG_FILTER_VALUE_UP-1] = png_read_filter_row_up_sse2;
-		if (bpp == 4) {
-			pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub4_ssse3;
-			pp->read_filter[PNG_FILTER_VALUE_AVG-1] = png_read_filter_row_avg4_ssse3;
-			pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth4_ssse3;
-		} else if (bpp == 3) {
-			pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub3_ssse3;
-			pp->read_filter[PNG_FILTER_VALUE_AVG-1] = png_read_filter_row_avg3_ssse3;
-			pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth3_ssse3;
+		switch (bpp) {
+			case 3:
+				pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub3_ssse3;
+				pp->read_filter[PNG_FILTER_VALUE_AVG-1] = png_read_filter_row_avg3_ssse3;
+				pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth3_ssse3;
+				break;
+			case 4:
+				pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub4_ssse3;
+				pp->read_filter[PNG_FILTER_VALUE_AVG-1] = png_read_filter_row_avg4_ssse3;
+				pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth4_ssse3;
+				break;
+			case 6:
+				pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub6_ssse3;
+				pp->read_filter[PNG_FILTER_VALUE_AVG-1] = png_read_filter_row_avg6_ssse3;
+				pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth6_ssse3;
+				break;
+			case 8:
+				pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub8_ssse3;
+				pp->read_filter[PNG_FILTER_VALUE_AVG-1] = png_read_filter_row_avg8_sse2;
+				pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth8_ssse3;
+				break;
 		}
 	}
 }
