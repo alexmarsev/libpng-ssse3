54f42c621d44e4cc0a543a3b8d743c8f4880fe00
Alex Marsev <alex.marsev@gmail.com>
2013-01-31 12:41:16 +0300

    Add copyright/license header to yasm-libtool.sh

--- ssse3/yasm-libtool.sh
+++ ssse3/yasm-libtool.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# yasm-libtool.sh - libtool/yasm bridge script
+
+# Copyright (c) 2013 Alex Marsev
+
+# This code is released under the libpng license.
+# For conditions of distribution and use, see the disclaimer and license in png.h
 
 COMMAND=""
 while test $# -gt 0; do
