79bd8e03049d3f27c430dcdd9e7cb46ef6de5c4b
Alex Marsev <alex.marsev@gmail.com>
2013-01-31 19:27:04 +0300

    Make yasm-libtool.sh more failsafe

--- ssse3/yasm-libtool.sh
+++ ssse3/yasm-libtool.sh
@@ -8,7 +8,11 @@
 
 COMMAND=""
 while test $# -gt 0; do
-	test "$1" = "-fPIC" || COMMAND="$COMMAND $1"
+	case "$1" in
+		-felf*|-fwin*|-fmacho*) COMMAND="$COMMAND $1";;
+		-f*) ;;
+		*) COMMAND="$COMMAND $1";;
+	esac
 	shift
 done
 exec $COMMAND
